130c23e1ad381709f3f4d5048f75c67ae60fe14e
Yoshua Wuyts <yoshuawuyts@gmail.com>
2019-12-10 17:16:50 +0100

    add warnings
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

---
+++ src/warning.rs
@@ -0,0 +1,64 @@
+use annotate_snippets::display_list::DisplayList;
+use annotate_snippets::formatter::DisplayListFormatter;
+use annotate_snippets::snippet::{Annotation, AnnotationType, Slice, Snippet, SourceAnnotation};
+
+/// An error formatter.
+pub struct Warning {
+    snippet: Snippet,
+}
+
+impl Warning {
+    /// Create a new `Warning` formatter.
+    pub fn new(label: String) -> Self {
+        Self {
+            snippet: Snippet {
+                title: Some(Annotation {
+                    label: Some(label),
+                    id: None,
+                    annotation_type: AnnotationType::Warning,
+                }),
+                slices: vec![],
+                footer: vec![],
+            },
+        }
+    }
+
+    /// Pass a new error to the formatter.
+    pub fn error(
+        mut self,
+        line_start: usize,
+        start: usize,
+        end: usize,
+        source: String,
+        label: String,
+    ) -> Self {
+        self.snippet.slices.push(Slice {
+            source,
+            line_start,
+            origin: None,
+            fold: true,
+            annotations: vec![SourceAnnotation {
+                label,
+                annotation_type: AnnotationType::Warning,
+                range: (start, end),
+            }],
+        });
+        self
+    }
+
+    /// Create a new footer.
+    pub fn help(mut self, label: String) -> Self {
+        self.snippet.footer.push(Annotation {
+            label: Some(label),
+            id: None,
+            annotation_type: AnnotationType::Help,
+        });
+        self
+    }
+
+    pub fn to_string(self) -> String {
+        let dl = DisplayList::from(self.snippet);
+        let dlf = DisplayListFormatter::new(true, false);
+        format!("{}", dlf.format(&dl))
+    }
+}
