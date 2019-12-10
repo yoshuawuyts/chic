863c0d7d029f12487652f3ac02195c5e0872ea4e
Yoshua Wuyts <yoshuawuyts@gmail.com>
2019-12-10 17:39:59 +0100

    fix last example
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -11,9 +11,9 @@
 //! with the list of annotations below.
 //! "#;
 //!
-//! let msg = Error::new("expected type, found `x`".to_string())
-//!     .error(260, 0, 12, src.to_string(), "found `x`".to_string())
-//!     .help("try using a foobs intead".to_string())
+//! let msg = Error::new("expected type, found `x`")
+//!     .error(260, 0, 12, src, "found `x`")
+//!     .help("try using a foobs intead")
 //!     .to_string();
 //!
 //! println!("{}", msg);
